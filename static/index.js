c3a72cd3b380670b2623293292b66037bdf981a5
fgfobdpqjs <qiuqiucai001@gmail.com>
2025-07-30 19:39:50 +0800

    Add files via upload

--- static/index.js
+++ static/index.js
@@ -14,7 +14,7 @@ let __Time = 20;
 let __k = 4;
 let _close = false;
 let _fsj = false;
-var url = 'https://fgfobdpqjs.github.io/CHICXKgai1/static/image/ClickBefore.png';
+var url = './image/ClickBefore.png';
 
 function isplaying() {
     return document.getElementById('welcome').style.display == 'none' &&
